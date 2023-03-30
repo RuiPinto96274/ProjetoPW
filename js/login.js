e0a205db2f47c1c5ddae9f1632aa685e9161de2d
RuiPinto96274 <101046649+RuiPinto96274@users.noreply.github.com>
2023-03-30 21:48:47 +0100

    Alterações Login

--- js/login.js
+++ js/login.js
@@ -1,41 +1,77 @@
-const loginButtons = document.querySelectorAll('.login-button');
-const loginForm = document.querySelector('.login');
-const overlay = document.createElement('div');
-overlay.classList.add('overlay');
 
-loginButtons.forEach((button) => {
-    console.log(button); // add this line
-    button.addEventListener('click', () => {
-      // Store the previous page URL in localStorage
-      localStorage.setItem('previousPage', window.location.href);
-      loginForm.classList.add('show');
-      document.body.appendChild(overlay);
-    });
-  });
-
-loginButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    // Store the previous page URL in localStorage
-    localStorage.setItem('previousPage', window.location.href);
-    loginForm.classList.add('show');
-    document.body.appendChild(overlay);
-  });
-});
-
-overlay.addEventListener('click', () => {
-  loginForm.classList.remove('show');
-  document.body.removeChild(overlay);
-});
-
-function closeLogin() {
-  console.log(loginForm)
-  const previousPage = localStorage.getItem('previousPage');
-  if (previousPage) {
-    window.location.href = previousPage;
-  } else {
-    loginForm.classList.remove('show');
-  }
+function showRegisterForm(){
+  $('.loginBox').fadeOut('fast',function(){
+      $('.registerBox').fadeIn('fast');
+      $('.login-footer').fadeOut('fast',function(){
+          $('.register-footer').fadeIn('fast');
+      });
+      $('.modal-title').html('Registar');
+  }); 
+  $('.error').removeClass('alert alert-danger').html('');
+     
 }
+function showLoginForm(){
+  $('#loginModal .registerBox').fadeOut('fast',function(){
+      $('.loginBox').fadeIn('fast');
+      $('.register-footer').fadeOut('fast',function(){
+          $('.login-footer').fadeIn('fast');    
+      });
+      
+      $('.modal-title').html('Login');
+  });       
+   $('.error').removeClass('alert alert-danger').html(''); 
+}
+
+function openLoginModal(){
+  showLoginForm();
+  setTimeout(function(){
+      $('#loginModal').modal('show');    
+  }, 230);
+  
+}
+function openRegisterModal(){
+  showRegisterForm();
+  setTimeout(function(){
+      $('#loginModal').modal('show');    
+  }, 230);
+  
+}
+
+function loginAjax(){
+  /*   Remove this comments when moving to server
+  $.post( "/login", function( data ) {
+          if(data == 1){
+              window.location.replace("/home");            
+          } else {
+               shakeModal(); 
+          }
+      });
+  */
+
+/*   Simulate error message from the server   */
+   shakeModal();
+}
+
+function shakeModal(){
+  $('#loginModal .modal-dialog').addClass('shake');
+           $('.error').addClass('alert alert-danger').html("Combinação email/password inválida");
+           $('input[type="password"]').val('');
+           setTimeout( function(){ 
+              $('#loginModal .modal-dialog').removeClass('shake'); 
+  }, 1000 ); 
+}
+
+$(document).ready(function () {
+  // Fechar o modal quando o botão 'fechar' for clicado
+  $(".modal-header button").click(function () {
+    $("#loginModal").modal("hide");
+  });
+
+  // Limpar os campos do formulário quando o modal for fechado
+  $("#loginModal").on("hidden.bs.modal", function () {
+    $("#loginUser")[0].reset();
+  });
+});
 
 function handleSubmit(event) {
   event.preventDefault();
