d4cd980e98a4f2b576d7fcc2affc6eee0ac2c3e8
RuiPinto96274 <101046649+RuiPinto96274@users.noreply.github.com>
2023-04-22 00:40:52 +0100

    alterações login admin

--- js/login_admin.js
+++ js/login_admin.js
@@ -1,50 +1,3 @@
-/*function showAdminRegisterForm(){
-  $('.loginBox').fadeOut('fast',function(){
-      $('.registerBox').fadeIn('fast');
-      $('.login-footer').fadeOut('fast',function(){
-          $('.register-footer').fadeIn('fast');
-      });
-      $('.modal-title').html('Registar');
-  }); 
-  $('.error').removeClass('alert alert-danger').html('');
-}
-
-function openAdminRegisterModal(){
-  showAdminRegisterForm();
-  setTimeout(function(){
-      $('#adminLoginModal').modal('show');    
-  }, 230);
-}
-
-document.getElementById("createAdmin").addEventListener("submit", function(event){
-  // impede que a página recarregue quando o formulário é enviado
-  event.preventDefault();
-
-  // obter os valores do formulário
-  var email = $("#adminemailLogin").val();
-  var password = $("#adminpasswordLogin").val();
-
-  // armazenar os valores no localStorage
-  var admin = {
-    email: email,
-    password: password
-  };
-
-  // Valida se os campos estão preenchidos corretamente
-  if (email == '' || password == '') {
-    $('.error').addClass('alert alert-danger').html('Por favor preencha todos os campos.');
-  }else {
-  localStorage.setItem('admin', JSON.stringify(admin));
-  }
-
-  alert("Registo bem sucedido! Por favor, faça login.");
-
-  // limpa o formulário
-  document.getElementById("createAdmin").reset();
-
-  $('#adminLoginModal').modal('hide');
-});*/
-
 function showAdminLoginForm(){
   $('#adminLoginModal .registerBox').fadeOut('fast',function(){
       $('.loginBox').fadeIn('fast');
@@ -64,26 +17,39 @@ function openAdminLoginModal(){
   }, 230);
 }
 
+let admin = {
+  email: "admin@admin.com",
+  password: "1234"
+};
+
+// Cria o admin se ele ainda não existe
+if (!localStorage.getItem('admin')) {
+  localStorage.setItem('admin', JSON.stringify(admin));
+}
+
 document.getElementById("adminloginUser").addEventListener("submit", function(event){
   // impede que a página recarregue quando o formulário é enviado
   event.preventDefault();
 
-  var admins = [];
   // obter os dados do formulário
-  var email = $("#adminemailLogin").val();
-  var password = $("#adminpasswordLogin").val();
-
-  // verificar credenciais do usuário e autenticá-lo
-  var admin = JSON.parse(localStorage.getItem('admin'));
+  let email = $("#adminemailLogin").val();
+  let password = $("#adminpasswordLogin").val();
+  // verificar credenciais do admin e autenticá-lo
+  let admin = JSON.parse(localStorage.getItem('admin'));
+  console.log(email);
+  console.log(admin.password);
+  console.log(admin.email);
+  console.log(admin.password);
   if (admin && admin.email === email && admin.password === password) {
+
     // exibir mensagem de sucesso
     alert("Login bem sucedido!");
     // limpa o formulário
     document.getElementById("adminloginUser").reset();
 
     $('#adminLoginModal').modal('hide');
-    $("#loginBtnNav").hide();
-    $("#logoutBtnNav").show();
+    $("#loginBtnFooter").hide();
+    $("#logoutBtnFooter").show();
   } else {
     // exibir mensagem de erro
     alert("Credenciais inválidas. Por favor, verifique seu e-mail e senha e tente novamente.");
@@ -91,21 +57,11 @@ document.getElementById("adminloginUser").addEventListener("submit", function(ev
     document.getElementById("adminloginUser").reset();
     openAdminLoginModal();
   }
-
-  // adicionar o usuário ao array
-  var admin = {
-    email: email,
-    password: password
-  };
-  admins.push(admin);
-  // após o login bem sucedido
-  // fechar o modal
-  $("#adminLoginModal").modal("hide");
 });
 
-function closeLogin(){
-  $("#logoutBtnNav").hide();
-  $("#loginBtnNav").show();
+function closeAdminLogin(){
+  $("#logoutBtnFooter").hide();
+  $("#loginBtnFooter").show();
 }
 
 $(document).ready(function () {
@@ -119,4 +75,5 @@ $(document).ready(function () {
     $("#adminloginUser")[0].reset();
   });
 });
+
 
